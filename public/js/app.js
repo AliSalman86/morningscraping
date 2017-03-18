1911b49476e38ac21ccda577ab2dc6c14dd9d2c0
Ali Salman <Ali Salman>
2017-03-18 14:05:24 -0500

    adding buttons and functionality

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,8 @@
 $("#scrapper").on("click", function() {
     $.ajax({
         method: "GET",
-        url: "/scrape"
-    }).done(function() {
-        getArticles();
+        url: "/scrape",
+        success: getArticles
     });
 });
 
@@ -13,8 +12,24 @@ function getArticles() {
     $("#artWell").empty();
     $.getJSON("/articles", function(data) {
         for (var i = 0; i<data.length; i++) {
-            $("#artWell").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+            var panel = $("<div>").addClass("panel panel-success");
+            var panHeading = $("<div>").addClass("panel-heading panHeader");
+            var leftSide = $("<div>").addClass("col-xs-9");
+            var title = $("<h3>").addClass("panel-title title").text(data[i].title);
+            var rightSide = $("<div>").addClass("col-xs-3");
+            var btnSave = $("<button>").addClass("btn btn-primary save").text("Save Article");
+            var btnDelete = $("<button>").addClass("btn btn-danger delete").text("Delete Article");
+            var body = $("<div>").addClass("panel-body").text(data[i].link);
+            leftSide.append(title);
+            panHeading.append(leftSide);
+            rightSide.append(btnSave);
+            rightSide.append(btnDelete);
+            panHeading.append(rightSide);
+            panel.append(panHeading);
+            panel.append(body);
+            $("#artWell").append(panel);
         }
     });
 }
 
+getArticles();
